1959a9fcf523d5409691df5094fb5b433eea08bb
Vlad_Den <33760265+VladDen4@users.noreply.github.com>
2024-01-08 16:40:04 +0300

    Create StringExtensions

---
+++ TiseShortUrl/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TiseShortUrl.Extensions;
+
+public static class StringExtensions
+{
+    public static string CorrectStringLength(this string source, int maxLength = 20)
+    {
+        if (source?.Length < maxLength)
+        {
+            return source;
+        }
+        else
+        {
+            return source![..(maxLength / 2)] + "..." + source![(source!.Length - maxLength / 4)..];
+        }
+    }
+}
--- TiseShortUrl/Models/ShortUrl.cs
+++ TiseShortUrl/Models/ShortUrl.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using TiseShortUrl.Extensions;
 
 namespace TiseShortUrl.Models;
 
@@ -14,6 +15,22 @@ public class ShortUrl()
     [Display(Name = "Short URL")]
     public string Url { get; set; } = string.Empty;
 };
+
+    public string ShortName
+    {
+        get
+        {
+            return Name.CorrectStringLength(25);
+        }
+    }
+
+    public string ShortOriginalUrl
+    {
+        get
+        {
+            return OriginalUrl.CorrectStringLength(40);
+        }
+    }
 
 // OwnerId
 // CreatedAt
